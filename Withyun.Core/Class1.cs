6d4ee25acd0f92b0f7bf5c98e99066359585be94
kinshines <1176807665@qq.com>
2018-08-01 23:18:51 +0800

    architecture

---
+++ Withyun.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Withyun.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Withyun.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Withyun.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
