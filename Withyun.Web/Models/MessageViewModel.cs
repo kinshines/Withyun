fdad70daeaf3c40415c245c5665271b5b42ec94e
kinshines <1176807665@qq.com>
2018-08-05 20:49:07 +0800

    models

---
+++ Withyun.Web/Models/MessageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Withyun.Web.Models
+{
+    public class MessageViewModel
+    {
+        [Required]
+        [Display(Name = "留言")]
+        public string Message { get; set; }
+        public string Contacts { get; set; }
+    }
+}
---
+++ Withyun.Web/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Withyun.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
