88a89dfcdd0aa7edf4bc5f9f4028de72c839986a
kinshines <1176807665@qq.com>
2018-08-05 20:36:41 +0800

    cookie

--- Withyun.Web/Startup.cs
+++ Withyun.Web/Startup.cs
@@ -30,13 +30,6 @@ namespace Withyun.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             Logger.logger = LoggerFactory.CreateLogger<Logger>();
             services.AddSingleton<EmailService>();
             services.AddSingleton<SearchService>();
