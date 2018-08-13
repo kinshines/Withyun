a5cd5159d56bb9cc45506bd2db10aec97ff17aca
kinshines <1176807665@qq.com>
2018-08-13 20:48:59 +0800

    component

---
+++ Withyun.Web/Components/ProfileFollow.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Withyun.Infrastructure.Services;
+
+namespace Withyun.Web.Components
+{
+    public class ProfileFollow : ViewComponent
+    {
+        readonly FollowService _followService;
+        public ProfileFollow(FollowService followService)
+        {
+            _followService = followService;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            ViewBag.userId = id;
+            ViewBag.userName = _followService.GetDistributorName(id);
+            var userId = GetUserId();
+            ViewBag.followed = userId != 0 && _followService.Exist(id, userId);
+            return View();
+        }
+        private int GetUserId()
+        {
+            return Convert.ToInt32(((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+    }
+}
