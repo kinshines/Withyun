0cede0ef4dbdf26ac9705d3f8d73728bd003e053
kinshines <1176807665@qq.com>
2018-08-15 00:09:42 +0800

    recomment

---
+++ Withyun.Web/Components/Recomment.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Withyun.Core.Enums;
+using Withyun.Infrastructure.Services;
+
+namespace Withyun.Web.Components
+{
+    public class Recomment : ViewComponent
+    {
+        readonly RecommentService _recommentService;
+        public Recomment(RecommentService recommentService)
+        {
+            _recommentService = recommentService;
+        }
+
+        public IViewComponentResult Invoke(RecommentCategory id)
+        {
+            var list = _recommentService.GetRecommentsByCategory(id);
+            ViewBag.Title = id.ToString();
+            ViewBag.Icon = _recommentService.GetIconByCategory(id);
+            return View(list);
+        }
+    }
+}
